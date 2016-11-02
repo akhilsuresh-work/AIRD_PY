1de56e9cee5b6b9c6e5b5592ff3049c4241a569d
Akhil S <akhil.s@inapp.com>
2016-11-02 17:19:40 +0530

    index page editted

--- CMS/urls.py
+++ CMS/urls.py
@@ -20,6 +20,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^login/', include('login.urls')),
+    url(r'^aird/', include('login.urls')),
     url(r'^admin/', admin.site.urls),
 ]
